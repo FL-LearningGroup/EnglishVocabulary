e384c31a1905d1e6b49eb14f35117003f33e131b
Lucas Yao (Wicresoft North America) <v-diya@microsoft.com>
2021-02-25 18:18:37 +0800

    develop

---
+++ src/core/IWordLoader.cs
@@ -0,0 +1,15 @@
+﻿using EnglishVocabulary.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishVocabulary.Core
+{
+    public interface IWordLoader
+    {
+        List<string> LoadWordFromFile(WordStorageType type, string path);
+        Task LoadWordMp3FileAsync(Word word);
+    }
+}
---
+++ src/core/IWorkSpeaker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishVocabulary.Core
+{
+    public interface IWorkSpeaker
+    {
+        void Speak(string media);
+    }
+}
